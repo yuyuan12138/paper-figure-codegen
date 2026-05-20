5ad335248cf8d7b54bd32ecbb7989fe9c015c343
Ruojun Wang <yuyuan12138x@gmail.com>
2026-05-20 17:45:13 +0800

    feat: update package exports for public API

--- paper_figure_codegen/__init__.py
+++ paper_figure_codegen/__init__.py
@@ -1,3 +1,10 @@
 """Paper Figure Codegen - Publication-quality plotting code generator."""
 
 __version__ = "0.1.0"
+
+from paper_figure_codegen.core.color_system import PAPER_PALETTE, get_palette
+from paper_figure_codegen.core.data_parser import DataParser
+from paper_figure_codegen.core.data_spec import FigureDataSpec
+from paper_figure_codegen.core.style import apply_paper_style
+from paper_figure_codegen.export import save_figure
+from paper_figure_codegen.recipes import get_recipe, list_recipes
--- paper_figure_codegen/core/__init__.py
+++ paper_figure_codegen/core/__init__.py
@@ -1,1 +1,15 @@
 """Core modules for data parsing, color system, and style."""
+
+from paper_figure_codegen.core.color_system import PAPER_PALETTE, get_palette
+from paper_figure_codegen.core.data_parser import DataParser
+from paper_figure_codegen.core.data_spec import DATA_KINDS, FigureDataSpec
+from paper_figure_codegen.core.style import apply_paper_style
+
+__all__ = [
+    "PAPER_PALETTE",
+    "get_palette",
+    "DataParser",
+    "DATA_KINDS",
+    "FigureDataSpec",
+    "apply_paper_style",
+]
